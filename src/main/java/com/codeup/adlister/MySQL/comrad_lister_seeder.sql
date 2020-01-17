e8e45d9d688f78d17165d68c5f7e7a09866554cd
colinmarchbanks <marchbanks.colin@gmail.com>
2020-01-17 16:38:34 -0600

    Seeder complete

--- src/main/java/com/codeup/adlister/MySQL/comrad_lister_seeder.sql
+++ src/main/java/com/codeup/adlister/MySQL/comrad_lister_seeder.sql
@@ -1,5 +1,62 @@
 USE comrad_lister;
 
-INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Soviet Union','Eurasia', 10);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Grenada','South America', 7);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Zanzibar','Africa', 4);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Benin','Africa', 6);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Madagascar','Africa', 6);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Angola','Africa', 7);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Mozambique','Africa', 7);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Congo','Africa', 7);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Somalia','Africa', 7);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Yemen','Africa', 7);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Ethiopia','Africa', 7);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Mozambique','Africa', 7);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Iran','Africa', 3);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Afghanistan','Africa', 3);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Cambodia','Asia', 8);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Vietnam','Asia', 10);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Korea','Asia', 9);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('China','Asia', 10);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Spain','Europe', 1);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('France','Europe', 1);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Finland','Europe', 2);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Austria','Europe', 2);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Germany','Europe', 7);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Hungary','Europe', 8);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Czechoslovakia','Europe', 8);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Romania','Europe', 8);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Poland','Europe', 7);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Albania','Europe', 8);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Bulgaria','Europe', 8);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Yugoslavia','Europe', 8);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Mongolia','Asia', 9);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Azerbaijan','Asia', 5);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Cuba','North American', 10);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Lao','Asia', 7);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Colombia','South America', 6);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Nepal','Asia', 6);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('United States of America','North America', 0);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('USSR','Eurasia', 10);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Russia','Eurasia', 8);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Ukraine','Europe', 3);
+INSERT INTO countries(name, continent, level_of_comradery) VALUES ('Baltic States(Estonia, Latvia, Lithuania)','Eurasia', 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
