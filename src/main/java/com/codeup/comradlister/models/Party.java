809dcfc800696f7a2b3c21bbb72e2607c2e44c02
RiverRobins <rivergrahamrobins@gmail.com>
2020-01-21 11:23:15 -0600

    made parties dao

--- src/main/java/com/codeup/comradlister/models/Party.java
+++ src/main/java/com/codeup/comradlister/models/Party.java
@@ -1,8 +1,88 @@
 package com.codeup.comradlister.models;
 
 public class Party {
-    private int id;
+    private Long id;
     private String name;
     private String description;
-    private int
+    private String dateFounded;
+    private String dateDissolved;
+    private Long countryID;
+    private String flagUrl;
+
+    public Party(){}
+
+    public Party(Long id, String name, String description, String dateFounded, String dateDissolved, Long countryID, String flagUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.dateFounded = dateFounded;
+        this.dateDissolved = dateDissolved;
+        this.countryID = countryID;
+        this.flagUrl = flagUrl;
+    }
+
+    public Party(String name, String description, String dateFounded, String dateDissolved, Long countryID, String flagUrl) {
+        this.name = name;
+        this.description = description;
+        this.dateFounded = dateFounded;
+        this.dateDissolved = dateDissolved;
+        this.countryID = countryID;
+        this.flagUrl = flagUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateFounded() {
+        return dateFounded;
+    }
+
+    public void setDateFounded(String dateFounded) {
+        this.dateFounded = dateFounded;
+    }
+
+    public String getDateDissolved() {
+        return dateDissolved;
+    }
+
+    public void setDateDissolved(String dateDissolved) {
+        this.dateDissolved = dateDissolved;
+    }
+
+    public Long getCountryID() {
+        return countryID;
+    }
+
+    public void setCountryID(Long countryID) {
+        this.countryID = countryID;
+    }
+
+    public String getFlagUrl() {
+        return flagUrl;
+    }
+
+    public void setFlagUrl(String flagUrl) {
+        this.flagUrl = flagUrl;
+    }
 }
