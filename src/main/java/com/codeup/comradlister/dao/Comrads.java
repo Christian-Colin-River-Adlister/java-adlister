c20ed95e8450d20387d82b35dda4c724a5ce392a
colinmarchbanks <marchbanks.colin@gmail.com>
2020-01-21 11:44:20 -0600

    Parties from comrads completed

--- src/main/java/com/codeup/comradlister/dao/Comrads.java
+++ src/main/java/com/codeup/comradlister/dao/Comrads.java
@@ -1,6 +1,7 @@
 package com.codeup.comradlister.dao;
 
 import com.codeup.comradlister.models.Comrad;
+import com.codeup.comradlister.models.Party;
 import com.codeup.comradlister.models.User;
 
 import java.util.List;
@@ -9,5 +10,5 @@ public interface Comrads {
     Comrad findByName(String name);
     List<Comrad> all();
     Long insert(Comrad comrad);
-    List<Party> getParties();
+    List<Party> getComradeParties(Comrad comrad);
 }
