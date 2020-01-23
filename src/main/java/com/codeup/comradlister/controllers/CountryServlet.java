b11c3ffc1cdc1fb74fff07fda63a4456bdafef89
ChristianCrousser <christian.crousserkaiman@gmail.com>
2020-01-23 15:59:53 -0600

    Added country servlet

--- src/main/java/com/codeup/comradlister/controllers/CountryServlet.java
+++ src/main/java/com/codeup/comradlister/controllers/CountryServlet.java
@@ -9,14 +9,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "controllers.ComradServlet", urlPatterns = "/comrade")
-public class ComradServlet extends HttpServlet {
+@WebServlet(name = "controllers.CountriesIndexServlet", urlPatterns = "/country")
+public class CountryServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.setAttribute("country", DaoFactory.getCountriesDao().findByName(request.getParameter("name")));
+//        request.setAttribute("countries", DaoFactory.getCountriesDao().all());
+        request.getRequestDispatcher("/WEB-INF/comrades/countries.jsp").forward(request, response);
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("comrade", DaoFactory.getComradsDao().findByName(request.getParameter("name")));
-//        request.setAttribute("users", DaoFactory.getUsersDao().all());
-        request.getRequestDispatcher("/WEB-INF/comrades/comrades.jsp").forward(request, response);
+
     }
 }
